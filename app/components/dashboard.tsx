2496250b8e9dbef7a863a4b7b502b9c0b25964e9
wiztemple <wiztemple7@gmail.com>
2025-04-19 14:56:26 +0100

    fix breaking build

--- app/components/dashboard.tsx
+++ app/components/dashboard.tsx
@@ -18,29 +18,31 @@ interface DashboardProps {
 
 export function Dashboard({ claims }: DashboardProps) {
   return (
-    <div className="container mx-auto space-y-6">
-      <DashboardHeader />
+    <div className="w-full space-y-6 sm:px-0 px-4 bg-white dark:bg-gray-900">
+      <div className="container mx-auto bg-white dark:bg-gray-900">
+        <DashboardHeader />
 
-      <div className="">
-        <DashboardSummary claims={claims} />
+        <div className="">
+          <DashboardSummary claims={claims} />
+        </div>
+
+        <Card className="border border-gray-200 shadow-none dark:border-gray-800 bg-white dark:bg-gray-900">
+          <CardContent>
+            <ClaimsTable claims={claims} />
+          </CardContent>
+        </Card>
+
+        <Card className="shadow-none border border-gray-100 dark:border-gray-800 bg-white dark:bg-gray-900">
+          <CardHeader className="pb-2">
+            <CardTitle className="text-gray-800 dark:text-gray-200">
+              Revenue Forecasting
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <RevenueForecasting claims={claims} />
+          </CardContent>
+        </Card>
       </div>
-
-      <Card className="border border-gray-200 shadow-none dark:border-gray-800 bg-white dark:bg-gray-900">
-        <CardContent>
-          <ClaimsTable claims={claims} />
-        </CardContent>
-      </Card>
-
-      <Card className="shadow-none border border-gray-100 dark:border-gray-800 bg-white dark:bg-gray-900">
-        <CardHeader className="pb-2">
-          <CardTitle className="text-gray-800 dark:text-gray-200">
-            Revenue Forecasting
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <RevenueForecasting claims={claims} />
-        </CardContent>
-      </Card>
     </div>
   );
 }
